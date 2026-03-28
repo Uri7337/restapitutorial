7968398691526b4ece5e6870b76dc13bdfe4a7ff
uri <uri@uri.cz>
2026-03-28 20:02:55 +0100

    feat(): api key in gitignore

--- src/main/java/uri/tech/RestApiTutorial.java
+++ src/main/java/uri/tech/RestApiTutorial.java
@@ -1,7 +1,18 @@
 package uri.tech;
 
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 public class RestApiTutorial {
     public static void main(String[] args) {
-        System.err.println("hi");
+        
+        String uri1 = "https://http.cat/";
+
+        HttpRequest postRequest = HttpRequest.newBuilder()
+            .uri(new URI(uri1))
+            //different class takes api key from file and puts it here
+            //.header("Authorization", "here->apikey")
+            
+
     }
 }
